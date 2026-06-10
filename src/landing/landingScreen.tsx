a55766feacef6d6079f2d6f0b44528d7696d871b
mridhops <mridhophs@gmail.com>
2026-06-10 13:56:43 +0700

    test action

--- src/landing/landingScreen.tsx
+++ src/landing/landingScreen.tsx
@@ -2,7 +2,7 @@ export default function LandingScreen() {
   return (
     <div className="w-full h-full">
       <div className="w-full bg-blue-600">
-
+        <h1>Hello Worlds</h1>
       </div>
     </div>
   )
