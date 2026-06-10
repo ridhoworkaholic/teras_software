4834819d8a489e898c2d3b3de25391196bb67834
mridhops <mridhophs@gmail.com>
2026-06-10 22:00:08 +0700

    change style new

--- src/landing/landingScreen.tsx
+++ src/landing/landingScreen.tsx
@@ -1,9 +1,10 @@
-export default function LandingScreen() {
+export default function landingScreen() {
   return (
     <div className="w-full h-full">
       <div className="w-full bg-blue-600">
         <h1>Hello Worlds</h1>
         <h2>New Push and automatic pull</h2>
+        <h2>New Push and automatic pull 2</h2>
       </div>
     </div>
   )
