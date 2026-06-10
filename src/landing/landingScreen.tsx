13988123503351c27508570157d272ae7b199c2b
mridhops <mridhophs@gmail.com>
2026-06-10 22:07:22 +0700

    change style new

--- src/landing/landingScreen.tsx
+++ src/landing/landingScreen.tsx
@@ -5,7 +5,8 @@ export default function landingScreen() {
         <h1>Hello Worlds</h1>
         <h2>New Push and automatic pull</h2>
         <h2>New Push and automatic pull 2</h2>
+        <h2>New Push and automatic pull 3</h2>
       </div>
     </div>
-  )
+  );
 }
